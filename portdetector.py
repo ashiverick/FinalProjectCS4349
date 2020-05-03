e0d2b53b0b6f58c62ceb783c643347291a4aa2c4
ashiverick <ashiverick@smu.edu>
2020-05-03 17:38:33 -0400

    Final Push

--- portdetector.py
+++ portdetector.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
+#Alden Shiverick 
+#CS4349
 
 import dpkt
-import sys #, socket,  random
-
+import sys 
 from datetime import datetime
 
 
 try:
-    file = open('test-2.pcap', 'rb') 
+    file = open('test-2.pcap', 'rb')            #change "test-2.pcap" to the pcap file you want to test with, include location of the file if it is not in this folder
     pcap = dpkt.pcap.Reader(file)
     print("PCAP file opened and read \n")
 except (IOError, KeyError):
-    print("Could not open file!")
-    sys.exit(-1)
+    print("Could not open file!")               #if you recieve this error output in the console the file is not properly opening
+    sys.exit(-1)                                #make sure file location is correct and there are not typos 
 
 inc = 0
 ts1 = 0
@@ -20,18 +21,24 @@ ts2 = 0
 firsttimestamp = 0
 lowestport = 1000000
 highestport = 0
+scanduration = 0
+totaltime = 0
+avtime = 0
 
-print("##########################################################\n")
-print("#1 Scan\n")
 
 for ts, buf in pcap:
+    print("##########################################################\n")
+    print("#", inc+1, " Scan\n")
+
     if inc == 0:
         firsttimestamp = ts
-        print("First time stamp: ", firsttimestamp)
-    try:
-        eth = dpkt.ethernet.Ethernet(buf)
-    except (dpkt.dpkt.Unpack.Error, IndexError):
-        continue
+        ts2 = ts
+        #print("First time stamp: ", firsttimestamp)
+    
+    eth = dpkt.ethernet.Ethernet(buf)
+    # except (dpkt.dpkt.Unpack.Error, IndexError):
+    #     print("*******************************************************Is this code totally useless?")
+    #     continue
 
     ip = eth.data
     if not ip:
@@ -56,23 +63,19 @@ for ts, buf in pcap:
         lowestport = tcpdport
 
     ts1 = ts
+    scanduration = ts1-ts2
     
-    print("Source Port: ", tcpsport, " Destination Port: ", tcpdport, " At Timestamp: ", datetime.fromtimestamp(ts), " For Duration: ", ts1-ts2, '\n')
+    print("Source Port: ", tcpsport, " Destination Port: ", tcpdport, " At Timestamp: ", datetime.fromtimestamp(ts), " Duration of Scan: ", scanduration, '\n')
     
     inc += 1
+    totaltime += scanduration
     ts2 = ts1
     print('\n')
-    print("##########################################################\n")
-    print("#", inc, " Scan\n")
-    #print(ts, len(buf))
 
 print('\n')
 print("##########################################################\n")
 print('\n')
-print("Ethernet: ",  eth, '\n')
-
-# for ts, buf in pcap:
-#     eth = dpkt.ethernet.Ethernet(buf)
+#print("Ethernet: ",  eth, '\n')
 
 
 ip = eth.data
@@ -81,11 +84,13 @@ tcpsport = tcp.sport
 tcpdport = tcp.dport
 datetime1 = datetime.fromtimestamp(firsttimestamp)
 datetime2 = datetime.fromtimestamp(ts)
+avtime = totaltime/inc
 
-print("IP: ", ip, " TCP: ", tcp, " TCP SPort: ", tcpsport, " TCP DPort: ", tcpdport, '\n')
+#print("IP: ", ip, " TCP: ", tcp, " TCP SPort: ", tcpsport, " TCP DPort: ", tcpdport, '\n')
 print("The ports span: ", lowestport, " to ", highestport, '\n')
 print("There were ", inc, " scans taken \n")
 print("The scans were taken between ", firsttimestamp, "and", ts, '\n')
 print("The scans took ", ts - firsttimestamp, "seconds, or ", (ts-firsttimestamp)/60, " minutes \n")
 print("The scans were taken between: ", datetime1, "and", datetime2, '\n')
+print("Average time during scans: ", avtime, "seconds \n")
 
